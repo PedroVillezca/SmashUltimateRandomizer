c2c18cd9c60359532f6bb9ec14ea5f2ff0dd722d
Pedro Villezca <pedrovillezca@Pedros-MacBook-Pro.local>
2019-11-14 22:13:58 -0600

    Add character and skin randomization

--- public/_js/main.js
+++ public/_js/main.js
@@ -1,4 +1,14 @@
 let userSets
+let characterFolders = ["01", "02", "03", "04", "04e", "05", "06", "07", 
+						"08", "09", "10", "11", "12", "13", "13e", "14",
+						"15", "16", "17", "18", "19", "20", "21", "21e",
+						"22", "23", "24", "25", "25e", "26", "27",
+						"28", "28e", "29", "30", "31", "32", "33-35",
+						"36", "37", "38", "39", "40", "41", "42", "43",
+						"44", "45", "46", "47", "48", "49", "50", "51",
+						"52", "53", "54", "55", "56", "57", "58", "59",
+						"60", "60e", "61", "62", "63", "64", "65", "66",
+						"66e", "67", "68", "69", "70", "71"]
 /* ===== CURRENT SET ===== */
 // Arrays
 let characters
@@ -19,6 +29,7 @@ function startUp () {
 		window.location.href = './index.html'
 	}
 	getSets()
+	handleRandomizer()
 }
 
 function getSets () {
@@ -66,6 +77,13 @@ function handleSets () {
 	}
 }
 
+function handleRandomizer () {
+	let btRandomize = $('#btRandomize')
+	$(btRandomize).on('click', (e) => {
+		randomize()
+	})
+}
+
 function chooseSet (rset) {
 	characters = rset.characters
 	skinsOn = rset.skinsOn
@@ -91,6 +109,33 @@ function chooseSet (rset) {
 	currentTitle.html(rset.description)
 }
 
+function randomize () {
+	// Randomize
+	let enabledCharacters = []
+	for (let i = 0; i < characters.length; i++) {
+		if (characters[i]) {
+			enabledCharacters.push(i)
+		}
+	}
+	let characterIndex = enabledCharacters[randomIntFromInterval(0, enabledCharacters.length-1)]
+	let selectedCharacter = characterFolders[characterIndex]
+	let selectedSkin = 1
+	if (skinsOn) {
+		for (let i = 0; i < skins.length; i++) {
+			if (skins[i].character == characterIndex) {
+				selectedSkin = skins[i].enabled[randomIntFromInterval(0, skins[i].enabled.length-1)]
+			}
+		}
+	}
+
+	// Display results
+	let characterImg = $('#characterImg')
+	$(characterImg).attr('src', `./_assets/_renders/${selectedCharacter}/0${selectedSkin}.png`)
+}
+
+function randomIntFromInterval(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 console.log('Main page startup')
 startUp()
